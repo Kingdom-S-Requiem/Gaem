8fa20d95e91e6897f9a9e9595d43f717ca6823bc
Jayden Graap <34245723+Graap02@users.noreply.github.com>
2019-12-20 12:59:16 -0600

    Update for the break
    Do something over break ya weirdos

---
+++ finalPlay/finalPlay/Rooms and Movement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finalPlay
+{
+    using static System.Console;
+    class Rooms_and_Movement
+    {
+        bool playing = true;
+        public void rooms()
+        {
+            while (playing)
+            {
+                int room1 = 7;
+                int room2 = 12;
+                int room3 = 19;
+                int room4 = 24;
+                int bossRoom1 = 2;
+
+                Write("Welcome to hell, population: me. I really gotta get outta here. I guess I'll take some of this equipment with me."); //When the movement is implemented, the rest of this segment
+                //will be implemented as well. This is where you would choose your class.
+            }
+        }
+    }
+}
