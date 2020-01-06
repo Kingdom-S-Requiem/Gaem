08673171cb56ea7af27343103dac6200c5d001b5
Jayden Graap <34245723+Graap02@users.noreply.github.com>
2020-01-06 15:35:58 -0600

    It begins but this time I added more stuff
    UHHHHHHHHHHHH implementations of content

--- finalPlay/finalPlay/Program.cs
+++ finalPlay/finalPlay/Program.cs
@@ -6,11 +6,25 @@ using System.Threading.Tasks;
 
 namespace finalPlay
 {
+    using static System.Console;
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            WriteLine("Hello, I notice you ran this program. Does that mean that you want to play a game? (Yes/No)");
+            String userInput = ReadLine();
+            if (userInput == "Yes" || userInput == "yes")
+            {
+                WriteLine("Very well then, enjoy a wonderful story provided by the team. Venture on, young soul!");
+                Classes.displayClasses();
 
+            }
+            else if (userInput == "No" || userInput == "no")
+            {
+                WriteLine("No? Well in that case, leave! Some people actually want to play the game!");
+            }
+            else
+                WriteLine("You're not very good with these sorts of tasks, are you? Try again, and read carefully.");
         }
     }
 }
--- finalPlay/finalPlay/Rooms and Movement.cs
+++ finalPlay/finalPlay/Rooms and Movement.cs
@@ -9,19 +9,18 @@ namespace finalPlay
     using static System.Console;
     class Rooms_and_Movement
     {
-        bool playing = true;
-        public void rooms()
+        public static void displayRoomAndMovements()
         {
-            while (playing)
+            bool playing = true;
             {
-                int room1 = 7;
-                int room2 = 12;
-                int room3 = 19;
-                int room4 = 24;
-                int bossRoom1 = 2;
-
-                Write("Welcome to hell, population: me. I really gotta get outta here. I guess I'll take some of this equipment with me."); //When the movement is implemented, the rest of this segment
-                //will be implemented as well. This is where you would choose your class.
+                while (playing)
+                {
+                    int room1 = 7;
+                    int room2 = 12;
+                    int room3 = 19;
+                    int room4 = 24;
+                    int bossRoom1 = 2; 
+                }
             }
         }
     }
